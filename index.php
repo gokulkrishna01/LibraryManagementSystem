edf19b6989b113164f91fc19769e9d358e6afe97
gokulkrishna01 <gkgokulkrishna33@gmail.com>
2016-11-23 20:00:22 +0530

    Adding files

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+    <div class="container">
+		<div class="margin-top">
+			<div class="row">
+					<?php include('head.php'); ?>
+				<div class="span2">
+					<?php include('sidebar.php'); ?>
+				</div>
+				<div class="span10">
+					<?php include('slider.php'); ?>
+				</div>
+
+				<div class="span2">
+				<h4></h4>
+
+				</div>
+				<div class="span10">
+
+
+				<?php include('thumbnail.php'); ?>
+
+
+					<div class="text_content">
+					<div class="abc">
+					<!-- text content -->
+					<h4>Vision</h4>
+					<hr>
+					<p>
+					Amrita University has the mission to provide value-based education and mould the character of the younger generation through a system of wholesome learning, so that their earnest endeavour to achieve progress and prosperity in life is matched by an ardent desire to extend selfless service to society, one complementing the other. Thus, the university‘s activities are based upon a deep understanding of our values and culture. This is in alignment with the educational outlook of our Chancellor, namely, - "Education for life, not just education for a living."
+					</p>
+					</div>
+					</div>
+					<!-- end content -->
+
+
+				</div>
+
+			</div>
+		</div>
+    </div>
+<?php include('footer.php') ?>
---
+++ sidebar.php
@@ -0,0 +1,48 @@
+			<div class="life-side-bar">
+			<div class="hero-container">
+
+
+			</div>
+
+
+
+
+
+							<ul class="nav nav-tabs nav-stacked">
+						<li class="">
+						<a   href="#"><i class="icon-phone icon-large"></i>&nbsp;Contact US</a>
+						</li>
+					</ul>
+<strong>Address</strong>
+<p>Amrita Univeristy, Amritapuri, Valikavu </p>
+<p>Tel. nos.:</p>
+<p>0123456789</p>
+<p>Fax:</p>
+<p>*** </p>
+
+
+
+			</div>
+
+	<!-- vision student login -->
+	<div id="vision" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-header"><div class="alert alert-info"><strong>Vision</strong></div></div>
+		<div class="modal-body">
+		<p>					Amrita University has the mission to provide value-based education and mould the character of the younger generation through a system of wholesome learning, so that their earnest endeavour to achieve progress and prosperity in life is matched by an ardent desire to extend selfless service to society, one complementing the other. Thus, the university‘s activities are based upon a deep understanding of our values and culture. This is in alignment with the educational outlook of our Chancellor, namely, - "Education for life, not just education for a living."
+</p>
+		</div>
+		<div class="modal-footer">
+			<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i>&nbsp;Close</button>
+		</div>
+    </div>
+
+
+				    <!-- mission student login -->
+	<div id="mission" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-header"><div class="alert alert-info"><strong>Mission</strong></div></div>
+		<div class="modal-body">
+		</div>
+		<div class="modal-footer">
+			<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i>&nbsp;Close</button>
+		</div>
+    </div>
